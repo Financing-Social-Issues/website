d7f079899fa6d5dbf736ced6f268d1d5341e3960
Renato <renatoolivajunior@gmail.com>
2024-06-15 05:28:29 -0300

    Initial

--- inc/head.php
+++ inc/head.php
@@ -17,6 +17,22 @@
     <link rel="stylesheet" href="assets/css/main.min.css">
     <link rel="stylesheet" href="assets/css/bootstrap-icons.min.css">
 
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="https://socialissues.finance/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Financing Social SOIS">
+    <meta property="og:description" content="Transform the World with SOIS">
+    <meta property="og:image" content="https://socialissues.financepath/assets/facebook.jpg">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="socialissues.finance">
+    <meta property="twitter:url" content="https://socialissues.finance/">
+    <meta name="twitter:title" content="Financing Social SOIS">
+    <meta name="twitter:description" content="Transform the World with SOIS">
+    <meta name="twitter:image" content="https://socialissues.financepath/assets/twitter.jpg">
+
+    <link rel="canonical" href="https://socialissues.finance">
     <!-- Load google font
     ================================================== -->
     <script type="text/javascript">
